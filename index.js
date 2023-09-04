bff18be728ffada1179f310cbece5463c6cbbcf7
John <codebeast0420@gmail.com>
2023-09-04 08:12:35 -0700

    create main

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const Moralis = require("moralis").default;
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/tokenPrice", async (req, res) => {
+
+  console.log("here");
+
+  const { query } = req;
+
+  // Make the GET reques
+
+  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne
+  })
+
+  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo
+  })
+
+  const total = await Moralis.EvmApi.balance.getNativeBalance({
+    address: "0x4d7fcEB021ae8b2B98762a1eDbb88629D4f04F19",
+    chain: 0x38
+  })
+
+  const usdPrices = {
+    tokenOne: responseOne.raw.usdPrice,
+    tokenTwo: responseTwo.raw.usdPrice,
+    ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
+    total
+  }
+
+
+  return res.status(200).json(usdPrices);
+});
+
+Moralis.start({
+  apiKey: process.env.MORALIS_KEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Listening for API Calls`);
+  });
+});
